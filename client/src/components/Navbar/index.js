bb1c441539e26760782298c20822966c5c42da07
ashleyhodge <ashhodge@comcast.net>
2022-12-20 13:54:33 -0500

    navbar animation

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai"
 import logo from "../../assets/images/gingerly_knits_logo.png"
 
 const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
   return (
     <header>
       <nav>
         <div>
           {/* Logo */}
-          <div>
+          {/* <div>
             <img 
               className="h-[170px] w-auto sm:h-[220px]"
               src={logo}
               alt="Logo"
             />
-          </div>
-          {/* Nav dropdown */}
-          <div>
+          </div> */}
+          {/* Mobile Navbar */}
 
+          <div>
+                      {showNav ? (
+            <AiOutlineMenu
+              onClick={() => setShowNav(!showNav)}
+            />
+            ) : (
+            <AiOutlineMenu
+            onClick={() => setShowNav(!showNav)}
+            />
+          )}
+            <div className="flex justify-self-center">
+              <ul className={
+                (showNav ? "left-0" : "-left-full") +
+                " absolute transition-all duration-[0.8s] ease-in-out"
+              }>
+                <li className="">
+                  Home
+                </li>
+                <li>
+                  Shop
+                </li>
+                <li>
+                  Blog
+                </li>
+                <li>
+                  Make
+                </li>
+                <li>
+                  Contact
+                </li>
+              </ul>
+            </div>
           </div>
           {/* Login and cart elements */}
           <div>
