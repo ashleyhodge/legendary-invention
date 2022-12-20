2982e4900a8c8a71f3a683e0276bf21a9687eab1
ashleyhodge <ashhodge@comcast.net>
2022-12-20 12:45:40 -0500

    add images

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,9 +1,29 @@
 import React from "react";
+import logo from "../../assets/images/gingerly_knits_logo.png"
 
 const Navbar = () => {
   return (
     <header>
+      <nav>
+        <div>
+          {/* Logo */}
+          <div>
+            <img 
+              className="h-[170px] w-auto sm:h-[220px]"
+              src={logo}
+              alt="Logo"
+            />
+          </div>
+          {/* Nav dropdown */}
+          <div>
 
+          </div>
+          {/* Login and cart elements */}
+          <div>
+
+          </div>
+        </div>
+      </nav>
     </header>
   )
 }
