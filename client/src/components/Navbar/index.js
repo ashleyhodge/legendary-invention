12595b7995356a969aca4d00ac38b1e85e93cfaa
ashleyhodge <ashhodge@comcast.net>
2022-12-21 13:39:23 -0500

    add images and styling to mobile nav

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai"
 import logo from "../../assets/images/gingerly_knits_logo.png"
+import topNav from '../../assets/images/mobile_nav_1.png'
+import bottomNav from '../../assets/images/mobile_nav_2.png'
 
 const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
@@ -19,34 +21,50 @@ const Navbar = () => {
           {/* Mobile Navbar */}
 
           <div>
-                      {showNav ? (
-            <AiOutlineMenu
-              onClick={() => setShowNav(!showNav)}
-            />
-            ) : (
-            <AiOutlineMenu
-            onClick={() => setShowNav(!showNav)}
-            />
-          )}
-            <div className="flex justify-self-center">
+            {showNav ? (
+              <AiOutlineMenu
+                onClick={() => setShowNav(!showNav)}
+                className="sm:hidden "
+              />
+              ) : (
+              <AiOutlineMenu
+                onClick={() => setShowNav(!showNav)}
+                className="sm:hidden"
+              />
+            )}
+            <div className="">
               <ul className={
                 (showNav ? "left-0" : "-left-full") +
-                " absolute transition-all duration-[0.8s] ease-in-out"
+                " absolute transition-all duration-[1.0s] ease-in-out w-full h-full"
               }>
-                <li className="">
+                <li className="flex justify-center">
+                <img 
+                  src={topNav}
+                  alt="knit supplies"
+                  className="h-[220px] w-[240px] flex items-center"
+                  />
+                </li>
+                <li className="flex justify-center">
                   Home
                 </li>
-                <li>
+                <li className="flex justify-center">
                   Shop
                 </li>
-                <li>
+                <li className="flex justify-center">
                   Blog
                 </li>
-                <li>
+                <li className="flex justify-center">
                   Make
                 </li>
-                <li>
+                <li className="flex justify-center">
                   Contact
+                </li>
+                <li className="flex justify-center">
+                <img 
+                  src={bottomNav}
+                  alt="knit supplies"
+                  className="h-[220px] w-[240px] flex items-center"
+                  />
                 </li>
               </ul>
             </div>
