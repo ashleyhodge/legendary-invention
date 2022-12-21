8c39c99f6d1e162fbf72a976ab5bbf6f7423b8bc
ashleyhodge <ashhodge@comcast.net>
2022-12-21 14:03:26 -0500

    add desktop navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -9,72 +9,92 @@ const Navbar = () => {
   return (
     <header>
       <nav>
-        <div>
+        <div className="flex justify-between items-center">
           {/* Logo */}
-          {/* <div>
+          <div className="m-3">
             <img 
-              className="h-[170px] w-auto sm:h-[220px]"
+              className="hidden sm:flex h-[130px] w-auto sm:h-[180px]"
               src={logo}
               alt="Logo"
             />
-          </div> */}
-          {/* Mobile Navbar */}
-
-          <div>
-            {showNav ? (
-              <AiOutlineMenu
-                onClick={() => setShowNav(!showNav)}
-                className="sm:hidden "
-              />
-              ) : (
-              <AiOutlineMenu
-                onClick={() => setShowNav(!showNav)}
-                className="sm:hidden"
-              />
-            )}
-            <div className="">
-              <ul className={
-                (showNav ? "left-0" : "-left-full") +
-                " absolute transition-all duration-[1.0s] ease-in-out w-full h-full"
-              }>
-                <li className="flex justify-center">
-                <img 
-                  src={topNav}
-                  alt="knit supplies"
-                  className="h-[220px] w-[240px] flex items-center"
-                  />
-                </li>
-                <li className="flex justify-center">
+          </div>
+          
+            {/* Desktop Navbar */}
+            <div className="hidden sm:flex">
+              <ul className="flex">
+                <li>
                   Home
                 </li>
-                <li className="flex justify-center">
+                <li>
                   Shop
                 </li>
-                <li className="flex justify-center">
+                <li>
                   Blog
                 </li>
-                <li className="flex justify-center">
+                <li>
                   Make
                 </li>
-                <li className="flex justify-center">
+                <li>
                   Contact
-                </li>
-                <li className="flex justify-center">
-                <img 
-                  src={bottomNav}
-                  alt="knit supplies"
-                  className="h-[220px] w-[240px] flex items-center"
-                  />
                 </li>
               </ul>
             </div>
-          </div>
           {/* Login and cart elements */}
           <div>
 
           </div>
         </div>
       </nav>
+      {/* Mobile Navbar */}
+      <div>
+        {showNav ? (
+          <AiOutlineMenu
+            onClick={() => setShowNav(!showNav)}
+            className="sm:hidden w-7 h-7 m-3"
+          />
+          ) : (
+          <AiOutlineMenu
+            onClick={() => setShowNav(!showNav)}
+            className="sm:hidden w-7 h-7 m-3"
+          />
+        )}
+        <div className="">
+          <ul className={
+            (showNav ? "left-0" : "-left-full") +
+            " absolute transition-all duration-[1.0s] ease-in-out w-full h-full"
+          }>
+            <li className="flex justify-center">
+            <img 
+              src={topNav}
+              alt="knit supplies"
+              className="h-[220px] w-[240px]"
+              />
+            </li>
+            <li className="flex justify-center">
+              Home
+            </li>
+            <li className="flex justify-center">
+              Shop
+            </li>
+            <li className="flex justify-center">
+              Blog
+            </li>
+            <li className="flex justify-center">
+              Make
+            </li>
+            <li className="flex justify-center">
+              Contact
+            </li>
+            <li className="flex justify-center">
+            <img 
+              src={bottomNav}
+              alt="knit supplies"
+              className="h-[220px] w-[240px] flex items-center"
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
   )
 }
