1158a706b4a56062ad9246ec08e195276990cc8c
ashleyhodge <ashhodge@comcast.net>
2022-12-28 17:19:45 -0500

    add video section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,9 +18,18 @@ const Home = () => {
       <div>
         <HeroCarousel />
       </div>
-      <div>
-        <VideoPlayer />
+      <div className="sm:mt-[100px] font-bad-script font-black text-center">
+        <h2 className="sm:text-[32px] text-[#412117]">
+          Favorite Video Tutorials
+        </h2>
+        <p className="sm:text-[24px] sm:mx-[200px] text-[#8e4a45]">
+          These are simply videos on how to work cool stitches, learn new techniques and find new project ideas. If you have a video you'd like me to share, let me know!
+        </p>
+        <div className="mt-[20px]">
+          <VideoPlayer />
+        </div>
       </div>
+      
     </main>
   )
 }
