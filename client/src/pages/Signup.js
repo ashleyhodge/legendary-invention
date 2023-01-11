9887f487d7460bc4f12cb06f4f11bacbebf92a8c
ashleyhodge <ashhodge@comcast.net>
2023-01-11 14:39:45 -0500

    style sign up form

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -6,19 +6,63 @@ const Signup = () => {
       </h1>
       <div className="border-[3px] border-[#415C6B] opacity-[37%] rounded p-[10px] mx-[20%] my-[20px]">
         <form>
-          <div>
-            <div>
-              <label>
-                First Name
-              </label>
-              <input 
-                id="firstName"
-                name="First Name"
-                type="name"
-                className="border-2"
-              />
+          <div className="flex justify-center">
+            <div className="w-1/2">
+              <div className="flex">
+                <input 
+                  id="firstName"
+                  name="First Name"
+                  type="text"
+                  value="First Name"
+                  className="border-2 w-full p-1 m-2 rounded"
+                />
+                <input
+                  id="lastName"
+                  name="Last Name"
+                  type="text"
+                  value="Last Name"
+                  className="border-2 w-full p-1 m-2 rounded"
+                />
+              </div>
+              <div>
+                <input 
+                  id="username"
+                  name="username"
+                  type="text"
+                  value="Username"
+                  className="border-2 w-full p-1 m-2 rounded"
+                />
+              </div>
             </div>
-
+            <div className="ml-6">
+                <div>
+                <input 
+                  id="email"
+                  name="email"
+                  type="text"
+                  value="Email"
+                  className="border-2 p-1 m-2 rounded"
+                />
+              </div>
+              <div>
+                <input 
+                  id="password"
+                  name="password"
+                  type="text"
+                  value="Password"
+                  className="border-2 p-1 m-2 rounded"
+                />
+              </div>
+              <div>
+                <input 
+                  id="password"
+                  name="password"
+                  type="text"
+                  value="Repeat Password"
+                  className="border-2 p-1 m-2 rounded"
+                />
+              </div>
+            </div>
           </div>
         </form>
       </div>
