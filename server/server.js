ebd2c876f6331ba61904a3b9b41241a5eb1a1148
ashleyhodge <ashhodge@comcast.net>
2023-01-31 17:54:44 -0500

    add nodemailer to backend

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const nodemailer = require('nodemailer');
 const express = require('express');
 const { authMiddleware } = require('./utils/auth')
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const { cloudinary } = require('./utils/cloudinary');
+
+require('dotenv').config();
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -50,6 +53,59 @@ app.post('/api/upload', async (req, res) => {
   }
 })
 
+
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: process.env.EMAIL,
+    pass: process.env.WORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+  },
+});
+
+transporter.verify((err, success) => {
+  err
+    ? console.log(err)
+    : console.log(`=== Server is ready to take messages: ${success} ===`);
+});
+
+let mailOptions = {
+  from: "test@gmail.com",
+  to: process.env.EMAIL,
+  subject: "Nodemailer API",
+  text: "Hi from your nodemailer API",
+};
+
+app.post("/send", function (req, res) {
+  let mailOptions = {
+    from: "test@gmail.com",
+    to: process.env.EMAIL,
+    subject: "Nodemailer API",
+    text: "Hi from your nodemailer API",
+  };
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Email sent successfully");
+      res.json({ status: "Email sent" });
+    }
+  });
+});
+
+transporter.sendMail(mailOptions, function (err, data) {
+  if (err) {
+    console.log("Error " + err);
+  } else {
+    console.log("Email sent successfully");
+  }
+});
+
 // create a new instanceof an Apollo server with Graphql schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
@@ -72,6 +128,8 @@ app.get('*', (req, res) => {
       })
     })
   };
+
+  
 
 // call async function the start server
 startApolloServer(typeDefs, resolvers);
