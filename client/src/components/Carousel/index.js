ca53d5d7d7a1991b5d25bbef263b6e3256ff92c5
ashleyhodge <ashhodge@comcast.net>
2023-02-06 17:25:09 -0500

    deploy to heroku

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -1,45 +1,45 @@
-// import React from 'react';
-// import { Carousel } from '3d-react-carousal';
-// import AwesomeSlider from 'react-awesome-slider';
-// import withAutoplay from 'react-awesome-slider/dist/autoplay';
-// import 'react-awesome-slider/dist/styles.css';
-// import Shop from '../../assets/images/shopping.png';
-// import Make from '../../assets/images/making.png';
-// import Blog from '../../assets/images/blogging.png';
+import React from 'react';
+import { Carousel } from '3d-react-carousal';
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
+import Shop from '../../assets/images/shopping.png';
+import Make from '../../assets/images/making.png';
+import Blog from '../../assets/images/blogging.png';
 
 
-// const HeroCarousel = () => {
+const HeroCarousel = () => {
 
-//   let slides = [
-//     <img src={Shop} alt="shopping" className='w-[700px] h-[300px]'/>,
-//     <img src={Make} alt="making" className='w-[700px] h-[300px] '/>,
-//     <img src={Blog} alt="blogging" className='w-[700px] h-[300px]'/>,
-//   ];
+  let slides = [
+    <img src={Shop} alt="shopping" className='w-[700px] h-[300px]'/>,
+    <img src={Make} alt="making" className='w-[700px] h-[300px] '/>,
+    <img src={Blog} alt="blogging" className='w-[700px] h-[300px]'/>,
+  ];
 
-//   const AutoplaySlider = withAutoplay(AwesomeSlider);
+  const AutoplaySlider = withAutoplay(AwesomeSlider);
 
-//   return (
-//     <div>
-//       {/* Desktop View */}
-//       <div className='hidden sm:inline z-0'>
-//         <Carousel slides={slides} autoplay={true} interval={10000} arrows={false}/>
-//       </div>
-//       {/* Mobile View */}
-//       <div className='sm:hidden z-0'>
-//         <AutoplaySlider
-//           bullets={false}
-//           organicArrows={false}
-//           infinite={true}
-//           play={true}
-//           interval={10000}
-//         >
-//           <div data-src={Shop} />
-//           <div data-src={Make} />
-//           <div data-src={Blog} />
-//         </AutoplaySlider>
-//       </div>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      {/* Desktop View */}
+      <div className='hidden sm:inline z-0'>
+        <Carousel slides={slides} autoplay={true} interval={10000} arrows={false}/>
+      </div>
+      {/* Mobile View */}
+      <div className='sm:hidden z-0'>
+        <AutoplaySlider
+          bullets={false}
+          organicArrows={false}
+          infinite={true}
+          play={true}
+          interval={10000}
+        >
+          <div data-src={Shop} />
+          <div data-src={Make} />
+          <div data-src={Blog} />
+        </AutoplaySlider>
+      </div>
+    </div>
+  )
+}
 
-// export default HeroCarousel;
+export default HeroCarousel;
