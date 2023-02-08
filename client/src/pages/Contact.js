fb0240acadfda41714fc07c48b98a19f53b166d6
ashleyhodge <ashhodge@comcast.net>
2023-02-08 14:38:55 -0500

    nodemailer

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -19,7 +19,7 @@ const Contact = () => {
   const submitEmail = async (e) => {
     e.preventDefault();
     
-    await fetch("/send", {
+    await fetch("http://localhost:3001/send", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
